badb68ceee259a4029a842013fdc59ecead5b3fb
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-04-19 09:33:51 -0400

    change selection position

--- src/components/CohortList.jsx
+++ src/components/CohortList.jsx
@@ -4,10 +4,10 @@ export default function CohortList({ cohortSelection, handleCohortChange, region
 
     return (
         <div className="cohortList__card">
-            <h2>Choose Pokemon by Type </h2>
-            {cohortSelection.map((cohort,i) => <li key={i}><input type="button" onClick={handleCohortChange} value={cohort} /></li>)}
             <h2>Choose Pokemon by Region </h2>
             {regionSelection.map((region,i) => <li key={i}><input type="button" onClick={handleRegionChange} value={region} /></li>)}
+            <h2>Choose Pokemon by Type </h2>
+            {cohortSelection.map((cohort,i) => <li key={i}><input type="button" onClick={handleCohortChange} value={cohort} /></li>)}
         </div>
     )
 }
