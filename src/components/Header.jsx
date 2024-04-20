102ce90bc94f9356c41622cb1ce06495af5645ff
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-04-19 22:54:07 -0400

    add alex's name to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     return (
       <>
         <header>
-            <h1>Pokedex</h1>
+            <h1>Alexander's Pokedex</h1>
         </header>
             <div className="circle"><button></button></div>
       </>  
